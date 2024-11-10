b97ecbcb1183df4f5f9a20fbc04779e40a13a309
azimiHamid <azimihamid2003@gmail.com>
2024-11-11 02:22:34 +0500

    update the TravelApp by adding new features

--- projects/src/components/project1/TravelApp.jsx
+++ projects/src/components/project1/TravelApp.jsx
@@ -1,21 +1,36 @@
+/* eslint-disable react/prop-types */
+
 import { useState } from "react";
 
-/* eslint-disable react/prop-types */
-const initalItems = [
-  { id: 1, description: "Passports", qauntity: 2, packed: false },
-  { id: 2, description: "Socks", qauntity: 12, packed: false },
-  { id: 3, description: "Charger", qauntity: 8, packed: true },
-];
-
 function TravelApp() {
+  const [items, setItems] = useState([]);
+
+  const handleAddItems = (item) => {
+    setItems((items) => [...items, item]);
+  };
+  const handleDeleteItem = (id) => {
+    setItems((items) => items.filter((item) => item.id !== id));
+  };
+  const handleToggleItem = (id) => {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  };
+
   return (
     <section className="w-full min-h-screen flex flex-col items-center bg-[#F19721] text-[#0f003ddb] font-poppins">
       <Logo />
-      <Form />
+      <Form onAddItems={handleAddItems} />
       <div className="flex flex-col flex-grow w-full">
-        <PackingList />
+        <PackingList
+          items={items}
+          onDeleteItem={handleDeleteItem}
+          onToggleItem={handleToggleItem}
+        />
       </div>
-      <Stats />
+      <Stats items={items} />
     </section>
   );
 }
@@ -32,21 +47,18 @@ const Logo = () => {
   );
 };
 
-const Form = () => {
+const Form = ({ onAddItems }) => {
   const [description, setDescription] = useState("");
   const [qauntity, setQauntity] = useState(1);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!description) return;
-    const newItem = {
-      id: Date.now(),
-      description: description,
-      qauntity: qauntity,
-      packed: false,
-    };
+    const newItem = { id: Date.now(), description, qauntity, packed: false };
     console.log(newItem);
-    initalItems.push(newItem);
+
+    onAddItems(newItem);
+
     setDescription("");
     setQauntity(1);
   };
@@ -85,12 +97,17 @@ const Form = () => {
   );
 };
 
-const PackingList = () => {
+const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
   return (
     <article className="bg-[#4D3524] w-full p-4 md:p-6 lg:p-8 xl:px-16 flex flex-col gap-4 flex-grow">
-      <ul className="flex flex-wrap gap-3 justify-center md:justify-start">
-        {initalItems.map((item) => (
-          <Item key={item.id} item={item} />
+      <ul className="flex flex-col sm:flex-row sm:flex-wrap gap-2 sm:gap-6">
+        {items.map((item) => (
+          <Item
+            key={item.id}
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
         ))}
       </ul>
       <div className="mt-auto font-poppins text-[#4D3524] flex justify-end gap-4">
@@ -107,27 +124,67 @@ const PackingList = () => {
   );
 };
 
-const Stats = () => {
+const Stats = ({ items }) => {
+  if (items.length === 0) {
+    return (
+      <footer className="font-poppins italic text-center text-[#4D3524] bg-[#68BEA1] w-full text-md md:text-xl flex items-center justify-center p-4 md:py-7">
+        <em>Start adding some items to your packing list 🚀</em>
+      </footer>
+    );
+  }
+
+  const itemsPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((itemsPacked / items.length) * 100);
+
   return (
     <footer className="font-poppins italic text-center text-[#4D3524] bg-[#68BEA1] w-full text-md md:text-xl flex items-center justify-center p-4 md:py-7">
-      📦 You have {"4"} items on your list, and you already packed {"2"} 🚀
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : `📦 You have ${items.length} items on your list, and you already packed 
+          ${itemsPacked} - (${percentage}%) 🚀`}
+      </em>
     </footer>
   );
 };
 
-const Item = ({ item }) => {
+const Item = ({ item, onDeleteItem, onToggleItem }) => {
   return (
-    <li
-      className={`${
-        item.packed ? "line-through" : ""
-      } text-lg md:text-xl text-[#FDE7AA] flex items-center gap-2 md:gap-3`}
-    >
-      <input type="checkbox" id={item.id} />
-      <label htmlFor={item.id}>
-        {item.qauntity} {item.description}
+    <li className="text-lg md:text-xl text-[#FDE7AA] flex items-center justify-between sm:gap-1 py-1 px-[10px] rounded-3xl bg-[#022929]">
+      <label className="flex items-center w-full cursor-pointer space-x-2">
+        <input
+          type="checkbox"
+          id={item.id}
+          checked={item.packed}
+          onChange={() => {
+            onToggleItem(item.id);
+          }}
+          className="hidden peer"
+        />
+
+        <div className="w-5 h-5 rounded-md border-2 border-gray-300 peer-checked:bg-[#69C1A5] peer-checked:border-[#8bfeda] flex items-center justify-center transition duration-200">
+          <svg
+            className="hidden w-4 h-4 text-white peer-checked:block"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M5 13l4 4L19 7"
+            />
+          </svg>
+        </div>
+
+        <span className={`${item.packed ? "opacity-25" : ""} mx-2`}>
+          {item.qauntity} {item.description}
+        </span>
       </label>
+
       <span
-        onClick={() => null}
+        onClick={() => onDeleteItem(item.id)}
         className="text-sm md:text-base cursor-pointer"
       >
         ❌
