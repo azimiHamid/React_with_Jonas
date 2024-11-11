1c4395f33ac4da7e9d6c7c82a7f1d6edbdd1e32e
azimiHamid <azimihamid2003@gmail.com>
2024-11-11 14:18:47 +0500

    Add clear list functionality for the Travel App

--- projects/src/components/project1/TravelApp.jsx
+++ projects/src/components/project1/TravelApp.jsx
@@ -18,6 +18,11 @@ function TravelApp() {
       )
     );
   };
+  const handleClearItems = () => {
+    if (items.length === 0) return;
+    const conf = confirm("Clear all the items?");
+    if (conf === true) setItems([]);
+  };
 
   return (
     <section className="w-full min-h-screen flex flex-col items-center bg-[#F19721] text-[#0f003ddb] font-poppins">
@@ -28,6 +33,7 @@ function TravelApp() {
           items={items}
           onDeleteItem={handleDeleteItem}
           onToggleItem={handleToggleItem}
+          onClearItems={handleClearItems}
         />
       </div>
       <Stats items={items} />
@@ -40,7 +46,7 @@ const Logo = () => {
     <header className="p-4 lg:p-0 flex items-center justify-center text-[#4D3524]">
       <img
         src="/logo.png"
-        alt="Travel App Logo"
+        alt="Far Away Logo"
         className="w-80 md:w-96 lg:w-[500px]"
       />
     </header>
@@ -97,11 +103,26 @@ const Form = ({ onAddItems }) => {
   );
 };
 
-const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
+const PackingList = ({ items, onDeleteItem, onToggleItem, onClearItems }) => {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <article className="bg-[#4D3524] w-full p-4 md:p-6 lg:p-8 xl:px-16 flex flex-col gap-4 flex-grow">
       <ul className="flex flex-col sm:flex-row sm:flex-wrap gap-2 sm:gap-6">
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             key={item.id}
             item={item}
@@ -111,12 +132,19 @@ const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
         ))}
       </ul>
       <div className="mt-auto font-poppins text-[#4D3524] flex justify-end gap-4">
-        <select className="bg-[#FDE7AA] rounded-full px-3 py-1 w-full md:w-auto">
-          <option value="">SORT BY INPUT ORDER</option>
-          <option value="">SORT BY DESCRIPTION</option>
-          <option value="">SORT BY PACKED STATUS</option>
+        <select
+          onChange={(e) => setSortBy(e.target.value)}
+          value={sortBy}
+          className="bg-[#FDE7AA] rounded-full px-3 py-1 w-full md:w-auto"
+        >
+          <option value="input">SORT BY INPUT ORDER</option>
+          <option value="description">SORT BY NAME</option>
+          <option value="packed">SORT BY PACKED STATUS</option>
         </select>
-        <button className="bg-[#FDE7AA] active:scale-95 rounded-full px-3 py-1 w-full md:w-auto">
+        <button
+          onClick={onClearItems}
+          className="bg-[#FDE7AA] active:scale-95 rounded-full px-3 py-1 w-full md:w-auto"
+        >
           CLEAR LIST
         </button>
       </div>
