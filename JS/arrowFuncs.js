b7588ca6c0ded8ad276a16c221bcb0d824060514
azimiHamid <azimihamid2003@gmail.com>
2024-11-14 15:14:24 +0500

    Updates

--- JS/arrowFuncs.js
+++ JS/arrowFuncs.js
@@ -11,7 +11,7 @@ const { title, publicationDate, author, hasMovieAdaptation } = book;
 // arrow function
 const getYear = (str) => str.split("-")[0];
 
-const summary = `${title} is a book which was writte by ${author} at ${
-  getYear(publicationDate)
-}. The book has ${hasMovieAdaptation ? "" : "not"}been adapted as movie.`;
+const summary = `${title} is a book which was writte by ${author} at ${getYear(
+  publicationDate
+)}. The book has ${hasMovieAdaptation ? "" : "not "}been adapted as movie.`;
 console.log(summary);
