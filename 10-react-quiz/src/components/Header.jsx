677b2a556a47bb1be1806e60cd8b0d31cf7d1248
azimiHamid <azimihamid2003@gmail.com>
2024-12-12 10:07:46 +0500

    modify components to be responsive

--- 10-react-quiz/src/components/Header.jsx
+++ 10-react-quiz/src/components/Header.jsx
@@ -4,11 +4,11 @@ function Header() {
   return (
     <header className="flex flex-col items-center gap-5 p-4 sm:flex-row sm:justify-center md:gap-10">
       <img
-        className="w-24 sm:w-28 md:w-40 lg:w-48"
+        className="w-24 sm:w-28 md:w-40 lg:w-44"
         src={ReactLogo}
         alt="react-logo"
       />
-      <h1 className="text-gray-300 text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold md:font-normal font-ralewayDots text-center md:text-left">
+      <h1 className="text-gray-50 text-4xl sm:text-6xl md:text-7xl lg:text-8xl 2xl:text-9xl xl:uppercase font-bold md:font-semibold lg:font-medium font-ralewayDots text-center md:text-left">
         The React Quiz
       </h1>
     </header>
--- 10-react-quiz/src/components/StartScreen.jsx
+++ 10-react-quiz/src/components/StartScreen.jsx
@@ -3,8 +3,8 @@ function StartScreen({ numOfQuestions }) {
   return (
     <div className="flex flex-col items-center text-white px-6 mt-10">
       <div className="text-center">
-        <h2 className="text-4xl font-bold mb-4">Welcome to the React Quiz</h2>
-        <h3 className="text-xl mb-6">
+        <h2 className="text-3xl sm:text-5xl md:text-5xl md:font-normal xl:font-medium font-semibold mb-4">Welcome to the React Quiz</h2>
+        <h3 className="text-xl sm:text-3xl font-medium mb-6">
           {numOfQuestions} questions to test your React mastery!
         </h3>
         <button className="px-10 py-3 bg-gray-700 text-white font-medium rounded-full shadow-md hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-blue-300">
