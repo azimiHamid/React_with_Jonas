4545a449e905b5a069db9655a54fd841d142542f
azimiHamid <azimihamid2003@gmail.com>
2024-12-14 20:20:12 +0500

    Add the restart btn for the quiz app

--- 10-react-quiz/src/components/FinishScreen.jsx
+++ 10-react-quiz/src/components/FinishScreen.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-function FinishScreen({ points, maxPossiblePoints, highscore }) {
+function FinishScreen({ points, maxPossiblePoints, highscore, dispatch }) {
   const percentage = (points / maxPossiblePoints) * 100;
 
   let emoji;
@@ -10,13 +10,19 @@ function FinishScreen({ points, maxPossiblePoints, highscore }) {
   if (percentage === 0) emoji = "🤦‍♂️";
 
   return (
-    <div>
-      <p className=" my-14 py-5 px-8 sm:px-32 sm:text-lg rounded-full text-gray-50 bg-teal-600">
+    <div className="mt-14 lg:mt-28 flex flex-col justify-center items-center ">
+      <p className="py-5 px-8 sm:px-32 sm:text-lg rounded-full text-gray-50 bg-teal-600">
         <span className="text-2xl">{emoji}</span> You scored{" "}
         <strong>{points}</strong> out of {maxPossiblePoints} (
         {Math.ceil(percentage)}%)
       </p>
-      <p>(Highscore: {highscore} points)</p>
+      <p className="my-10 text-2xl">(Highscore: {highscore} points)</p>
+      <button
+        className="px-12 py-3 text-xl bg-gray-700 text-white font-medium rounded-full shadow-md hover:bg-gray-800 focus:outline-none"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart quiz
+      </button>
     </div>
   );
 }
