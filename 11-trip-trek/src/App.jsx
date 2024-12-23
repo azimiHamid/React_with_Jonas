cb24668e07708f333ee3f78b4e9eb5b5c47f7e47
azimiHamid <azimihamid2003@gmail.com>
2024-12-23 17:50:11 +0500

    add nested routes for the AppLayout page

--- 11-trip-trek/src/App.jsx
+++ 11-trip-trek/src/App.jsx
@@ -14,7 +14,10 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>List of countries</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
