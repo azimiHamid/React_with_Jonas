60363336b3540d73f4a86a41de77110507cf5c26
azimiHamid <azimihamid2003@gmail.com>
2025-01-01 22:54:35 +0500

    Add the Login functionality to the app using a Fake_User

--- 11-world-wise/src/pages/AppLayout.jsx
+++ 11-world-wise/src/pages/AppLayout.jsx
@@ -1,12 +1,14 @@
-import Sidebar from "../components/Sidebar";
-import Map from "../components/Map";
 import styles from "./AppLayout.module.css";
+import Sidebar from "../components/Sidebar";
+import User from "../components/User";
+import Map from "../components/Map";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 }
