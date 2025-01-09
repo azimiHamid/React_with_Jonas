a01b80363706fa63683fb07e71f6089651733e89
azimiHamid <azimihamid2003@gmail.com>
2025-01-09 16:29:21 +0500

    updates

--- fast-react-pizza/src/features/menu/Menu.jsx
+++ fast-react-pizza/src/features/menu/Menu.jsx
@@ -1,5 +1,23 @@
+/* eslint-disable react-refresh/only-export-components */
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData(); // Accesses data fetched by the router's loader function
+
+  return (
+    <ul className="grid gap-6 p-4 sm:grid-cols-2 sm:p-8 xl:grid-cols-3 lg:p-16 xl:px-24">
+      {menu.map((pizza) => (
+        <MenuItem key={pizza.id} pizza={pizza} />
+      ))}
+    </ul>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
--- fast-react-pizza/src/features/menu/MenuItem.jsx
+++ fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,14 +1,36 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { formatCurrency } from "../../utils/helpers";
+
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
-        <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+    <li className="flex flex-col items-center gap-4 p-4 bg-white rounded-lg shadow-md md:flex-row md:gap-6 cursor-pointer transition-all duration-300 hover:scale-105">
+      {/* Pizza Image */}
+      <img
+        src={imageUrl}
+        alt={name}
+        className="w-full h-40 object-cover rounded-lg md:w-32 md:h-32"
+      />
+
+      {/* Pizza Info */}
+      <div className="flex flex-col items-center text-center md:items-start md:text-left">
+        {/* Pizza Name */}
+        <p className="text-lg font-semibold text-gray-800">{name}</p>
+
+        {/* Ingredients */}
+        <p className="text-sm text-gray-600 mt-2">{ingredients.join(", ")}</p>
+
+        {/* Price or Sold Out */}
+        <div className="mt-4">
+          {!soldOut ? (
+            <p className="text-green-600 font-medium">
+              {formatCurrency(unitPrice)}
+            </p>
+          ) : (
+            <p className="text-red-500 font-medium">Sold Out</p>
+          )}
         </div>
       </div>
     </li>
