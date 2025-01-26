4ca1bd2e706d0b9bb253f86936033d688b630f4d
azimiHamid <azimihamid2003@gmail.com>
2025-01-26 17:58:17 +0500

    updates

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,9 +1,38 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
+import GlobalStyles from "./styles/GlobalStyles";
+const AppLayout = lazy(() => import("./ui/AppLayout"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Bookings = lazy(() => import("./pages/Bookings"));
+const Cabins = lazy(() => import("./pages/Cabins"));
+const Users = lazy(() => import("./pages/Users"));
+const Settings = lazy(() => import("./pages/Settings"));
+const Account = lazy(() => import("./pages/Account"));
+const Login = lazy(() => import("./pages/Login"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+
 function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <GlobalStyles />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
 }
 
-export default App
+export default App;
